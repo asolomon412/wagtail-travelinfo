7cb1d59b3bd20f5295679b0503dfd769dfd8ced6
Antonella Solomon <antonellasolomon@gmail.com>
2022-06-06 15:14:02 -0400

    added some streams

--- home/models.py
+++ home/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
 
 
 class HomePage(Page):
@@ -37,9 +40,14 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
     )
 
+    body = StreamField([
+        ("title", blocks.TitleBlock())
+    ], null = True, blank = True)
+
     content_panels = Page.content_panels + [
         FieldPanel("lead_text"),
         PageChooserPanel("button"),
         FieldPanel("button_text"),
         ImageChooserPanel("banner_background_image"),
+        StreamFieldPanel("body"),
     ]
